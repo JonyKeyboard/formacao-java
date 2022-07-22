77d574ce026a0a529d8f6a9f4fcfd543583bc660
Stanley <1jony2jony@gmail.com>
2022-07-22 11:58:52 -0300

    Aula 03 - Configurando visibilidade

---
+++ poo-curso-em-video/src/aula03/Aula03.java
@@ -0,0 +1,15 @@
+package aula03;
+
+public class Aula03 {
+
+	public static void main(String[] args) {
+		
+		Caneta c1 = new Caneta();
+		c1.modelo = "BIC";
+		c1.cor = "Vermelha";
+		c1.tampar();
+		c1.status();
+		
+	}
+	
+}
